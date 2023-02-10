256f4181d1ded9b03047584b17f91d7eabde9a84
eastzoo <dongju010@kakao.com>
2023-02-10 21:00:04 +0900

    ADD : 모델파일 생성, 타입 정의

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
   // private를 사용한 이유 : 사용하지 않으면 다른컴포넌트에서 boards 값에 접근가능하기 때문에
-  private boards = [];
+  private boards: Board[] = [];
 
-  getAllBoards() {
+  //getAllBoards 함수 역시 this를 통해 위의 boards를 반환하기 때문에 타입스크립트 반환 타입은 같다.
+  getAllBoards(): Board[] {
     return this.boards;
   }
 }
