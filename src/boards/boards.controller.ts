3cd1ad68ee7b90ee1b09248d03ebb242c51bfe19
eastzoo <dongju010@kakao.com>
2023-02-10 21:18:26 +0900

    ADD : CRUD - C 게시물 생성하는 기능 샘플

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
@@ -9,5 +9,14 @@ export class BoardsController {
   @Get('/')
   getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    // 여기는 Post를 통해 하나만 날리는 리턴 값이기 때문에 모두 가져올때 사용하는 [] 을 쓰지 않아도됨
+    return this.boardsService.createBoard(title, description);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
-
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 @Injectable()
 export class BoardsService {
   // private를 사용한 이유 : 사용하지 않으면 다른컴포넌트에서 boards 값에 접근가능하기 때문에
@@ -9,5 +9,18 @@ export class BoardsService {
   //getAllBoards 함수 역시 this를 통해 위의 boards를 반환하기 때문에 타입스크립트 반환 타입은 같다.
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  // 게시글 생성하는 기능
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(), // 유니크한 값 생성 라이브러리
+      title: title,
+      description: description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
