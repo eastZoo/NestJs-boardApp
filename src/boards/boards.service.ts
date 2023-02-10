edf9063919c1394204c161803f0e6361a29f983e
eastzoo <dongju010@kakao.com>
2023-02-10 20:50:14 +0900

    ADD : CRUD - R 서비스 구현

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  // private를 사용한 이유 : 사용하지 않으면 다른컴포넌트에서 boards 값에 접근가능하기 때문에
+  private boards = [];
+
+  getAllBoards() {
+    return this.boards;
+  }
+}
