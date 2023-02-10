57ed8d1775cd43fd2e00f9f7eb49d6f0a415ada1
eastzoo <dongju010@kakao.com>
2023-02-10 21:44:13 +0900

    ADD : 아이디로 특정 게시물 삭제하기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -31,4 +31,10 @@ export class BoardsService {
   getBoardById(id: string): Board {
     return this.boards.find((board) => board.id === id);
   }
+
+  // DELETE ID로 특정 게시물 삭제하기
+  deleteBoard(id: string): void {
+    // 삭제하는 로직이라 리턴값을 따로 주지 않을거기에 void (프로젝트에 따라 다름)
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
 }
