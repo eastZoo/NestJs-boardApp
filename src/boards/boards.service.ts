a80e783bf2264f2900afd4108c4522627a326a52
eastzoo <dongju010@kakao.com>
2023-02-14 21:39:53 +0900

    UPDATE : id로 특정 게시물 가져오기 DB이용

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,8 +2,25 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
+import { Board } from './board.entity';
 @Injectable()
 export class BoardsService {
+  constructor(
+    @InjectRepository(BoardRepository)
+    private boardRepository: BoardRepository,
+  ) {}
+
+  async getBoardById(id: number): Promise<Board> {
+    const found = await this.boardRepository.findOne(id);
+
+    if (!found) {
+      throw new NotFoundException("Can't find Board with ${}");
+    }
+
+    return found;
+  }
   // //getAllBoards 함수 역시 this를 통해 위의 boards를 반환하기 때문에 타입스크립트 반환 타입은 같다.
   // getAllBoards(): Board[] {
   //   return this.boards;
