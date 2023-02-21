101a93dc5001b9bda2ebe874262267ed306165af
eastzoo <dongju010@kakao.com>
2023-02-21 15:46:34 +0900

    ADD : 패스포트 이용해서 정보 가져오기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,8 +9,11 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { Req, UseGuards } from '@nestjs/common/decorators';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { GetUser } from './get-user.decorator';
 import { User } from './user.entity';
 
 @Controller('auth')
@@ -29,5 +32,11 @@ export class AuthController {
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.signInUser(authCredentialsDto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard('jwt'))
+  test(@GetUser() user: User) {
+    console.log('user', user);
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport/dist';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from 'src/boards/board.repository';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 
@@ -18,6 +20,9 @@ import { UserRepository } from './user.repository';
     TypeOrmModule.forFeature([User]),
   ],
   controllers: [AuthController],
-  providers: [AuthService, UserRepository],
+  //JwtStrategy, PassportModule를 이 Auth모듈에서 사용할 수 있게 등록
+  providers: [AuthService, UserRepository, PassportModule, JwtStrategy],
+  //JwtStrategy, PassportModule를 다른 모듈에서도 사용할 수 있게 등록
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
