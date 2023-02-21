dce0c5f1afb0cdd70d07463662598287a126143f
eastzoo <dongju010@kakao.com>
2023-02-21 16:43:47 +0900

    ADD : 유저,게시물 관계형성

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,10 +1,13 @@
+import { Board } from 'src/boards/board.entity';
 import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 
 @Entity()
 @Unique(['username'])
@@ -17,4 +20,13 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  // eager: true => 유저를 가져올때 게시물도 같이 가져올 수 있게.
+  @OneToMany((type) => Board, (board) => board.user, { eager: true })
+  boards: Board[];
+
+  async validatePassword(password: string): Promise<boolean> {
+    let isValid = await bcrypt.compare(password, this.password);
+    return isValid;
+  }
 }
--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -1,4 +1,11 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/auth/user.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { BoardStatus } from './board-status.enum';
 
 // CREATE TABLE board 부분을 의미한다.
@@ -16,4 +23,7 @@ export class Board extends BaseEntity {
 
   @Column()
   status: BoardStatus;
+
+  @ManyToOne((type) => User, (user) => user.boards, { eager: false })
+  user: User;
 }
